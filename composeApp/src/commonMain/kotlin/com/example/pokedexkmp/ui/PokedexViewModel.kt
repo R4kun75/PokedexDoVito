a3ed8d2e3b36e88d6047f47e7828546864675867
Vitor Cadorin Dalsasso <8322635@univali.br>
2026-04-06 20:04:46 -0300

    feat: mudando para mock mas mantendo a parte da API  para implementação posterior

--- composeApp/src/commonMain/kotlin/com/example/pokedexkmp/ui/PokedexViewModel.kt
+++ composeApp/src/commonMain/kotlin/com/example/pokedexkmp/ui/PokedexViewModel.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.pokedexkmp.data.PokeApiClient
 import com.example.pokedexkmp.data.Pokemon
+import com.example.pokedexkmp.data.PokemonMock
 import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.launch
 
@@ -24,23 +25,19 @@ class PokedexViewModel : ViewModel() {
     private val _isLoading = MutableStateFlow(true)
     val isLoading: StateFlow<Boolean> = _isLoading.asStateFlow()
 
-    // EVENTOS DE UI (Para disparar Snackbars/Avisos sem prender estado)
+    // EVENTOS DE UI (Para disparar Snackbars/o aviso de 6 sem prender estado)
     private val _uiEvent = MutableSharedFlow<String>()
     val uiEvent = _uiEvent.asSharedFlow()
 
-    init { fetchPokemons() }
+    init {
+        // Pega a lista do Mock
+        val listaMockada = PokemonMock.pokedex
 
-    private fun fetchPokemons() {
-        viewModelScope.launch {
-            _isLoading.value = true
-            // IDs variados dos pokemons, adicionando e removendo eles por aqui
-            val diverseIds = listOf(1, 4, 7, 25, 1006, 94, 133, 221, 149, 150)
+        // Passa a lista para o estado da UI
+        _pokedex.value = listaMockada
 
-            // Busca os 10 diretamente da API real de forma paralela!
-            val pokemons = apiClient.getPokemonsByIds(diverseIds)
-            _pokedex.value = pokemons
-            _isLoading.value = false
-        }
+        // Desliga a roda de loading
+        _isLoading.value = false
     }
 
     fun onSearchQueryChanged(query: String) {
